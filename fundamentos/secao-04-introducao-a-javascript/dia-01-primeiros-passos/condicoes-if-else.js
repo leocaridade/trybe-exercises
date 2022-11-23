45de72c6f43ff464029cb8e9616d3e6bcadbfa6d
leocaridade <leocaridade@hotmail.com>
2022-11-23 13:37:59 -0500

    Altera valores da variavel nota pra testar se o codigo funciona corretamente

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/condicoes-if-else.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/condicoes-if-else.js
@@ -1,7 +1,7 @@
-const nota = 65;
+const nota = 50;
 
 if (nota >= 80) {
-    console.log("Parabéns, você foi aprovado(o)");
+    console.log("Parabéns, você foi aprovada(o)");
 } else if (nota < 80 && nota >= 60) {
     console.log("Você está na nossa lista de espera");
 } else {
