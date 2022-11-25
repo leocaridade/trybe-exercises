ac1b3cbf4d736ff5b745453c85ab2eb5b15a2e4f
leocaridade <leocaridade@hotmail.com>
2022-11-25 16:52:20 -0500

    Resolve a parte de media aritmetica do exercicio

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/lidando-com-arrays.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/lidando-com-arrays.js
@@ -1,11 +1,23 @@
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// console.log(numbers);
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma = (soma + numbers[index]);
+// }
+// console.log(soma);
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
     soma = (soma + numbers[index]);
 }
-console.log(soma);
+media = (soma / numbers.length);
+console.log(media);
