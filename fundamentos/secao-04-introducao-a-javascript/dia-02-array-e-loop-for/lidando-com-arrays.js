64521b9674f1a3d6ab730df8607ce094d859d11c
leocaridade <leocaridade@hotmail.com>
2022-11-25 17:42:51 -0500

    Resolucao do exercicio, resultado da divisao de cada elemento por 2

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/lidando-com-arrays.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/lidando-com-arrays.js
@@ -52,12 +52,19 @@
 //     console.log(odd);
 // }
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let lowestNumber = numbers[0];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let lowestNumber = numbers[0];
 
-for (index = 1; index < numbers.length; index += 1) {
-    if (numbers[index] < lowestNumber) {
-        lowestNumber = numbers[index];
-    }
+// for (index = 1; index < numbers.length; index += 1) {
+//     if (numbers[index] < lowestNumber) {
+//         lowestNumber = numbers[index];
+//     }
+// }
+// console.log('O menor número é ' + lowestNumber);
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+for (let index = 0; index < numbers.length; index += 1) {
+        console.log(numbers[index] / 2);
 }
-console.log('O menor número é ' + lowestNumber);
+
