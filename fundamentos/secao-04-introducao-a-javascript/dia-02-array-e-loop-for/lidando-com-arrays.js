28a8fc6e7ea75a0313bc6a27b3af413df5b2a184
leocaridade <leocaridade@hotmail.com>
2022-11-25 17:03:16 -0500

    Resolvendo a parte do exercicio de qual o maior numero da array

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/lidando-com-arrays.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/lidando-com-arrays.js
@@ -12,17 +12,27 @@
 // }
 // console.log(soma);
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
-let media = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+// let media = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    soma = (soma + numbers[index]);
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma = (soma + numbers[index]);
+// }
+// media = (soma / numbers.length);
+// console.log(media);
+// if (media > 20) {
+//     console.log('Valor maior que 20');
+// } else {
+//     console.log('Valor menor ou igual a 20');
+// }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > higherNumber) {
+        higherNumber = numbers[index];
+    }
 }
-media = (soma / numbers.length);
-console.log(media);
-if (media > 20) {
-    console.log('Valor maior que 20');
-} else {
-    console.log('Valor menor ou igual a 20');
-}
+console.log(higherNumber);
