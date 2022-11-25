ee07d8f58330e009b04fee72b1e10c7ccbf5b8f4
leocaridade <leocaridade@hotmail.com>
2022-11-25 16:56:51 -0500

    Resolvendo a parte do exercicio se o numero e maior do que 20

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/lidando-com-arrays.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/lidando-com-arrays.js
@@ -21,3 +21,8 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 media = (soma / numbers.length);
 console.log(media);
+if (media > 20) {
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor ou igual a 20');
+}
