a349ef386aa99f3a77bff457c4002f2a4566bfb6
leocaridade <leocaridade@hotmail.com>
2022-11-29 13:17:56 -0500

    Resolucao da pratica 8

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/pratica8.js
@@ -0,0 +1,20 @@
+// const a = 81;
+// const b = 11;
+// const c = 23;
+
+// if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+const a = 1;
+const b = 8;
+const c = 5;
+
+let isEven = false;
+
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0)) {
+  isEven = true;
+};
+console.log(isEven);
