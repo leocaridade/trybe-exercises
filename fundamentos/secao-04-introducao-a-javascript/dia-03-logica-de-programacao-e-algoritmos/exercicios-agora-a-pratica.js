c9d3462b7d4c2586f69da914761e4c19dfab9ded
leocaridade <leocaridade@hotmail.com>
2022-11-29 09:16:33 -0500

    Resolucao do segundo exercicio da secao agora-a-pratica

--- fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmos/exercicios-agora-a-pratica.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmos/exercicios-agora-a-pratica.js
@@ -1,8 +1,19 @@
-//Crie um algoritmo que imprima na tela o fatorial de 10
-let fat = 1;
+// //Crie um algoritmo que imprima na tela o fatorial de 10
+// let fat = 1;
 
-for (let index = 10; index > 0; index -= 1) {
-    fat *= index;
+// for (let index = 10; index > 0; index -= 1) {
+//     fat *= index;
+// }
+
+// console.log(fat);
+
+
+//Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra.
+let word = 'tryber';
+let reverseWord = '';
+
+for (index = 0; index < word.length; index += 1) {
+    reverseWord += word[word.length - 1 - index];
 }
 
-console.log(fat);
+console.log(reverseWord);
