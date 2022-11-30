39ae933f6c5c85753e42311720219ee4a5c0d45d
leocaridade <leocaridade@hotmail.com>
2022-11-30 11:01:58 -0500

    Resolucao do exercicio bonus invertendo o lado do triangulo

--- fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmos/exercicios-bonus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmos/exercicios-bonus.js
@@ -15,12 +15,32 @@
 
 
 
-//Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+// //Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// for (let index = 0; index < n; index += 1) {
+//     inputLine += symbol;
+//     console.log(inputLine);
+// }
+
+
+//Agora inverta o lado do triângulo.
 let n = 5;
 let symbol = '*';
 let inputLine = '';
+let inputPosition = n - 1;
 
-for (let index = 0; index < n; index += 1) {
-    inputLine += symbol;
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+        if(columnIndex < inputPosition) {
+            inputLine += ' ';
+        } else {
+            inputLine += symbol;
+        }
+    }
     console.log(inputLine);
+    inputLine = '';
+    inputPosition -= 1;
 }
