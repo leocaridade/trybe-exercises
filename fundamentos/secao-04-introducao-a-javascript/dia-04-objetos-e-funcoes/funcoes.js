009f8c9ab6ab236c494e071f3dc24d9b9ed77511
leocaridade <leocaridade@hotmail.com>
2022-11-30 16:32:34 -0500

    Resolvendo exercicios do topico funcoes operacoes aritmeticas

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/funcoes.js
@@ -0,0 +1,28 @@
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(7, 3));
+
+
+function sub(a, b) {
+    return a - b;
+}
+console.log(sub(14, 5));
+
+
+function mult(a, b) {
+    return a * b;
+}
+console.log(mult(8, 3));
+
+
+function div(a, b) {
+    return a / b;
+}
+console.log(div(36, 6));
+
+
+function mod(a, b) {
+    return a % b;
+}
+console.log(mod(15, 5));
