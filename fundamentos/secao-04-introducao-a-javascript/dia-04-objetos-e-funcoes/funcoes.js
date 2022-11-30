80004dc018e15bda2270fd1c7c92dbdc7545ec24
leocaridade <leocaridade@hotmail.com>
2022-11-30 16:41:03 -0500

    Resolucao exercicio do topico funcoes - maior de tres numeros

--- fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/funcoes.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/funcoes.js
@@ -28,13 +28,25 @@
 // console.log(mod(15, 5));
 
 
-function biggest(a, b) {
-    if (a > b) {
-        return a + ' é maior do que ' + b;
-    } else if (a < b) {
-        return b + ' é maior do que ' + a;
-    } else {
-        return 'Os números são iguais'
+// function biggest(a, b) {
+//     if (a > b) {
+//         return a + ' é maior do que ' + b;
+//     } else if (a < b) {
+//         return b + ' é maior do que ' + a;
+//     } else {
+//         return 'Os números são iguais'
+//     }
+// }
+// console.log(biggest(22, 22));
+
+
+function biggest(a, b, c) {
+    if (a > b && a > c) {
+        return a + ' é o maior número'
+    } else if (b > a && b > c) {
+        return b + ' é o maior número'
+    } else if (c > a && c > b) {
+        return c + ' é o maior número'
     }
 }
-console.log(biggest(22, 22));
+console.log(biggest(23, 22, 18));
