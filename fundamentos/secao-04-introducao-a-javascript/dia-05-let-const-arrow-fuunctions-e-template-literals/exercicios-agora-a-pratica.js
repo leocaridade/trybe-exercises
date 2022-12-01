2f2aa796612685bcc2ee2c3032ca0bfc19e850cd
leocaridade <leocaridade@hotmail.com>
2022-12-01 16:35:20 -0500

    Resolucao dos exercicios 5, 6, 7 e 8 da secao agora-a-pratica

--- fundamentos/secao-04-introducao-a-javascript/dia-05-let-const-arrow-fuunctions-e-template-literals/exercicios-agora-a-pratica.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-let-const-arrow-fuunctions-e-template-literals/exercicios-agora-a-pratica.js
@@ -26,14 +26,41 @@
 
 
 
-    //Modifique as concatenações para template literals.
-    const name = 'Adriana';
-    const lastName = 'Soares';
-    console.log(`Olá, ${name} ${lastName}!`);
-    function soma(a,b) {
-      let resultado = a + b;
-      return resultado;
-    }
-    let a = 3;
-    let b = 5;
-    console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+    // //Modifique as concatenações para template literals.
+    // const name = 'Adriana';
+    // const lastName = 'Soares';
+    // console.log(`Olá, ${name} ${lastName}!`);
+    // function soma(a,b) {
+    //   let resultado = a + b;
+    //   return resultado;
+    // }
+    // let a = 3;
+    // let b = 5;
+    // console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+
+
+    // //Exercicio 5
+    // const numeroAleatorio = () => Math.random()
+
+    //   console.log(numeroAleatorio());
+
+
+    // //Exercicio 6
+    // const hello = (nome) => `Olá, ${nome}!`
+    //   let nome = 'Ivan';
+    //   console.log(hello(nome));
+
+
+    // //Exercicio 7
+    // const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+    //   let nome = 'Ivan';
+    //   let sobrenome = 'Pires';
+    //   console.log(nomeCompleto(nome, sobrenome));
+
+
+    //Exercicio 8
+    let speed = 70;
+    const speedCar = (speed) => (
+        speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`
+    );
+    console.log(speedCar(speed));
