6164cadecccb60f1aefeca1f24f42ec8de6578c5
leocaridade <leocaridade@hotmail.com>
2022-12-01 11:18:27 -0500

    Estudos sobre var, let e const

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-let-const-arrow-fuunctions-e-template-literals/var-let-const.js
@@ -0,0 +1,31 @@
+// function userInfo() {
+//     const userEmail = 'maria@email.com';
+  
+//     // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+    
+//   }
+//   userInfo();
+
+//   console.log(userEmail);
+
+
+// if (true) {
+//     // inicio do escopo do if
+//     var userAge = '20';
+//     console.log(userAge); // 20
+//     // fim do escopo do if
+//   }
+//   console.log(userAge); // 20
+
+// var userName = 'Isabella';
+// const userName = 'Lucas';
+// console.log(userName); // Resultado: Lucas
+
+
+const favoriteLanguage = 'Javascript';
+favoriteLanguage = 'Python';
+console.log(favoriteLanguage); // Erro
+
+let favoriteTechnology = 'Machine learning';
+favoriteTechnology = 'Facial recognition';
+console.log(favoriteTechnology); // Facial recognition
