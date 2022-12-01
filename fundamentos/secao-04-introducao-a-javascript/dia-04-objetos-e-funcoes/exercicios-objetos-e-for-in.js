a227619583d53cf9d841aa24d26032389cde4153
leocaridade <leocaridade@hotmail.com>
2022-12-01 08:16:19 -0500

    Resolucao do exercicio julia pessoa

--- fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-objetos-e-for-in.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-objetos-e-for-in.js
@@ -1,33 +1,50 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
-//   console.log('Bem-vinda, ' + info.personagem);
+// //   console.log('Bem-vinda, ' + info.personagem);
 
-  info['recorrente'] = 'Sim';
-//   console.log(info);
+//   info['recorrente'] = 'Sim';
+// //   console.log(info);
+
+// // for (let key in info) {
+// //     console.log(key);
+// // }
+
+// // for (let key in info) {
+// //     console.log(info[key]);
+// // }
+
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dells Four Color Comics',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+// };
 
 // for (let key in info) {
-//     console.log(key);
+//     if (info[key] != info2[key]) {    
+//     console.log(info[key] + ' e ' + info2[key]);
+//     } else {
+//         console.log('Ambos recorrentes');
+//     }
 // }
 
-// for (let key in info) {
-//     console.log(info[key]);
-// }
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dells Four Color Comics',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
 };
 
-for (let key in info) {
-    if (info[key] != info2[key]) {    
-    console.log(info[key] + ' e ' + info2[key]);
-    } else {
-        console.log('Ambos recorrentes');
-    }
-}
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + "'" + leitor.livrosFavoritos[0]['titulo'] + "'");
