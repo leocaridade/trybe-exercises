eb0d214fff9f3a425df5ed03c3ff8f92f0230fd6
leocaridade <leocaridade@hotmail.com>
2022-12-02 18:14:00 -0500

    Resolucao do exercicio bonus algarismo romano

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-bonus.js
@@ -0,0 +1,34 @@
+const numerosRomanos = {
+    i: 1,
+    v: 5,
+    x: 10,
+    l: 50,
+    c: 100,
+    d: 500,
+    m: 1000,
+};
+
+function romanoParaDecimal(numero) {
+    numero = numero.toLowerCase();
+    const len = numero.length;
+    let soma = numerosRomanos[numero[len - 1]];
+    let atual = numerosRomanos[numero[len - 1]];
+
+    for (let index = 2; index <= len; index += 1) {
+        const prox = numerosRomanos[numero[len - index]];
+
+        if (atual <= prox) {
+            soma += prox;
+        } else {
+            soma -= prox;
+        }
+
+        atual = prox;
+    }
+
+    return soma;
+}
+
+console.log(romanoParaDecimal('MMXVIII'));
+console.log(romanoParaDecimal('VI'));
+console.log(romanoParaDecimal('IV'));
