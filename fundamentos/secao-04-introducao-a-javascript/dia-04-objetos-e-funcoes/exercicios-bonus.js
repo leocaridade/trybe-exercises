4dadc8918fa7c923e3f5783ed06aa3024091fcd6
leocaridade <leocaridade@hotmail.com>
2022-12-02 21:16:10 -0500

    Resolucao do exercicio bonus basket

--- fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-bonus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-bonus.js
@@ -34,22 +34,62 @@
 // console.log(romanoParaDecimal('IV'));
 
 
-//Exercicio 2
-let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
-const arrayOfNumbers = vector => {
-    const result = [];
-    for (let index = 0; index < vector.length; index += 1) {
-        const numbers = vector[index];
+// //Exercicio 2
+// let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+// const arrayOfNumbers = vector => {
+//     const result = [];
+//     for (let index = 0; index < vector.length; index += 1) {
+//         const numbers = vector[index];
 
-        for (let indexSub = 0; indexSub < numbers.length; indexSub += 1) {
-            const current = numbers[indexSub];
+//         for (let indexSub = 0; indexSub < numbers.length; indexSub += 1) {
+//             const current = numbers[indexSub];
 
-            if ((current % 2) === 0) {
-                result.push(current);
-            }
-        }
-    }
+//             if ((current % 2) === 0) {
+//                 result.push(current);
+//             }
+//         }
+//     }
 
-    return result;
-}
-console.log(arrayOfNumbers(vector));
+//     return result;
+// }
+// console.log(arrayOfNumbers(vector));
+
+
+//Exercicio 3
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+  ];
+  
+  const result = {};
+  
+  for (let index = 0; index < basket.length; index += 1) {
+    const fruit = basket[index];
+    if (!result[fruit]) result[fruit] = 0;
+    result[fruit] += 1;
+  }
+  
+  const summaries = [];
+  
+  for (fruit in result) {
+    let message = `${result[fruit]} ${fruit}`;
+    if (result[fruit] > 1) message += 's';
+    summaries.push(message);
+  }
+  
+  console.log(`Sua cesta possui: ${summaries.join(', ')}.`);
