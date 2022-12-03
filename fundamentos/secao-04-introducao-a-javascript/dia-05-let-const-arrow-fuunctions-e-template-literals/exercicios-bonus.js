63abd43ba636ad6f59895118fb5f60257d164b02
leocaridade <leocaridade@hotmail.com>
2022-12-02 20:40:43 -0500

    Resolucao do exercicio bonus minhasSkills

--- fundamentos/secao-04-introducao-a-javascript/dia-05-let-const-arrow-fuunctions-e-template-literals/exercicios-bonus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-let-const-arrow-fuunctions-e-template-literals/exercicios-bonus.js
@@ -7,5 +7,18 @@ const substituaX = nome => {
         }
     }
     return fraseArray.join(' ');
-}
-console.log(substituaX('Leonardo'));
+};
+//console.log(substituaX('Leonardo'));
+
+const minhasSkills = func => {
+    const skills = ['HTML', 'CSS', 'JavaScript'];
+    let result = `
+    ${func}
+    Minhas três principais habilidades são:`;
+    for (let index = 0; index < skills.length; index += 1) {
+        result = `${result} 
+        - ${skills[index]}`;
+    }
+    return result;
+};
+console.log(minhasSkills(substituaX('Leonardo')));
