b1334ae0a9297a0b78bbfe3511bfaab20538c635
leocaridade <leocaridade@hotmail.com>
2022-12-07 16:38:31 -0500

    Altera cor de fundo das divs, footer e h1

---
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-dom-e-seletores/script.js
@@ -0,0 +1,5 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'lightsalmon';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+document.getElementById('footer-container').style.backgroundColor = 'darkgreen';
+document.querySelectorAll('.emergency-tasks h3').style.backgroundColor = 'purple';
