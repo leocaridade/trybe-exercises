1f74d558d43ee2fe56cbee994d37dd6d7eb44947
leocaridade <leocaridade@hotmail.com>
2023-01-23 08:20:03 -0500

    Resolve o exercicio 3

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-coberturas-de-codigo/printMessage.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-coberturas-de-codigo/printMessage.test.js
@@ -3,4 +3,13 @@ describe('a função printMessage', () => {
     it('Espera que o objeto passado como parâmetro possua a propriedade "personagem"', () => {
         expect(info).toHaveProperty('personagem');
     });
+
+    it('Verifica se a funcao contem mensagem de boas vindas', () => {
+        expect(printMessage(info)).toMatch(/Boas vindas/);
+    });
+
+    it('Testa o retorno da funcao', () => {
+        expect(printMessage(info)).toMatch(/Margarida/);
+        expect(printMessage(info)).toMatch(info.personagem);
+    });
 });
