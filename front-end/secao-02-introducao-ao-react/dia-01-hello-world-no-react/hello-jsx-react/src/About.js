86d510c9f8fa05374992fd8fba360d07bd7e206f
leocaridade <leocaridade@hotmail.com>
2023-02-27 16:40:54 -0500

    Resolve os exercicios de fixacao

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/hello-jsx-react/src/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Leonardo Lindo</h1>
+        <p>Meu nome é Leonardo, tenho 34 anos e moro no Canadá com minha esposa e nossos 2 gatos (Peanut e Butter)</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Git</li>
+          <li>GitHub</li>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default About;
