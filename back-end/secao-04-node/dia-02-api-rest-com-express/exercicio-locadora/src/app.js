25463bbfe27b042b0ce669c40fa29de307b85911
leocaridade <leocaridade@hotmail.com>
2023-06-14 09:27:44 -0400

    Resolve os exercicios 8 e 9

--- back-end/secao-04-node/dia-02-api-rest-com-express/exercicio-locadora/src/app.js
+++ back-end/secao-04-node/dia-02-api-rest-com-express/exercicio-locadora/src/app.js
@@ -54,4 +54,35 @@ app.post('/movies', async (req, res) => {
   }
 });
 
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const index = movies.findIndex((element) => element.id === Number(id));
+
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
+
+    res.status(200).json(movies[index]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movies = await readFile();
+    const filteredMovies = movies.filter((movie) => movie.id !== Number(id));
+
+    const updatedMovies = JSON.stringify(filteredMovies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(204).end();
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
 module.exports = app;
