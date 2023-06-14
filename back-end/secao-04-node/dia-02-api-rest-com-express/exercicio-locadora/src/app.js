8dcbf8af88c51ac0aee491a8aa6a7fbb30c71323
leocaridade <leocaridade@hotmail.com>
2023-06-14 08:57:22 -0400

    Resolve os exercicios 3 a 7

---
+++ back-end/secao-04-node/dia-02-api-rest-com-express/exercicio-locadora/src/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.log(`Arquivo não pode ser lido: ${err.message}`);
+  }
+};
+
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const movies = await readFile();
+    const movie = movies.find((m) => m.id === Number(id));
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await readFile();
+    res.status(200).json(movies);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json(newMovie);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+module.exports = app;
