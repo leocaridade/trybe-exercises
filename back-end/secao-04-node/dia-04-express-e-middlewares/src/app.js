50746796912de5550c7c0c7927c0041bb269d29c
leocaridade <leocaridade@hotmail.com>
2023-06-16 11:26:52 -0400

    Resolve os exercicios 1 e 2 da segunda parte

--- back-end/secao-04-node/dia-04-express-e-middlewares/src/app.js
+++ back-end/secao-04-node/dia-04-express-e-middlewares/src/app.js
@@ -5,6 +5,8 @@ const isValidDescription = require('./middlewares/isValidDescription');
 const isValidCreatedAt = require('./middlewares/isValidCreatedAt');
 const isValidRating = require('./middlewares/isValidRating');
 const isValidDifficulty = require('./middlewares/isValidDifficulty');
+const generateToken = require('./utils/generateToken');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
@@ -17,7 +19,20 @@ app.post('/activities',
   isValidCreatedAt,
   isValidRating,
   isValidDifficulty,
+  auth,
   (req, res) => res.status(201)
   .json({ message: 'Atividade cadastrada com sucesso!' }));
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+
+  const token = generateToken();
+
+  return res.status(200).json({ token });
+});
 
 module.exports = app;
