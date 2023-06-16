acf90aa925297c2b177d9c2c2e3a63ae6f75f398
leocaridade <leocaridade@hotmail.com>
2023-06-16 11:10:21 -0400

    Resolve os exercicios de 1 a 9

--- back-end/secao-04-node/dia-04-express-e-middlewares/src/app.js
+++ back-end/secao-04-node/dia-04-express-e-middlewares/src/app.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const isValidName = require('./middlewares/isValidName');
+const isValidPrice = require('./middlewares/isValidPrice');
+const isValidDescription = require('./middlewares/isValidDescription');
+const isValidCreatedAt = require('./middlewares/isValidCreatedAt');
+const isValidRating = require('./middlewares/isValidRating');
+const isValidDifficulty = require('./middlewares/isValidDifficulty');
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/activities',
+  isValidName, 
+  isValidPrice,
+  isValidDescription,
+  isValidCreatedAt,
+  isValidRating,
+  isValidDifficulty,
+  (req, res) => res.status(201)
+  .json({ message: 'Atividade cadastrada com sucesso!' }));
 
 module.exports = app;
